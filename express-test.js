06c52db8916bb61ee227e8449fad00cd1b1838cf
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-07 11:25:54 -0300

    Change port to 80.

--- express-test.js
+++ express-test.js
@@ -1,6 +1,8 @@
+// ssh -i "device-manager.pem" ec2-user@ec2-54-94-254-219.sa-east-1.compute.amazonaws.com
+
 const express = require('express')
 const app = express()
-const port = 9000
+const port = 80
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
