e66677a6b05825cd5e45bf80f4ed32bc4858144c
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-07 12:18:54 -0300

    Change port.

--- express-test.js
+++ express-test.js
@@ -1,8 +1,9 @@
 // ssh -i "device-manager.pem" ec2-user@ec2-54-94-254-219.sa-east-1.compute.amazonaws.com
+// sudo "$(which node)" express-test.js
 
 const express = require('express')
 const app = express()
-const port = 80
+const port = 9000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
