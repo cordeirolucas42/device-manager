40bafe65f61e1bf16b69cae845432264119adcec
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-07 11:17:53 -0300

    Configured ports in AWS and changed port in code.

--- express-test.js
+++ express-test.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 9000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, "54.94.254.219", () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, () => {
+  console.log(`Example app listening at ec2-54-94-254-219.sa-east-1.compute.amazonaws.com:${port}`)
 })
