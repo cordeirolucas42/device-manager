e083ca3569d37b1d8bcecb64ec1d56361539b014
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-07 12:52:59 -0300

    Added some info about the test process.

--- express-test.js
+++ express-test.js
@@ -1,5 +1,10 @@
 // ssh -i "device-manager.pem" ec2-user@ec2-54-94-254-219.sa-east-1.compute.amazonaws.com
 // sudo "$(which node)" express-test.js
+// nohup node test1.js
+// rm nohup.out
+// ps -ef
+// kill [process num]
+// https://medium.com/cs-note/setup-node-and-express-on-aws-ec2-windows-7-8cb499ab14eb
 
 const express = require('express')
 const app = express()
@@ -7,13 +12,13 @@ const port = 9000
 
 app.get('/:user', (req, res) => {
     if (req.params.user) {
-        res.send(`Olá, ${req.params.user}! Bem-vinde!`)
+        res.send(`Olá, ${req.params.user}! Bem-vindo!`)
     } else {
-        res.send(`Olá! Bem-vinde!`)
+        res.send(`Olá! Bem-vindo!`)
     }
   
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at ec2-54-94-254-219.sa-east-1.compute.amazonaws.com:${port}`)
+  console.log(`Example app listening at http://ec2-54-94-254-219.sa-east-1.compute.amazonaws.com:${port}`)
 })
